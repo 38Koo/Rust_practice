0473ad9d814a782c1245c74b24dd199a8165cb8b
宮本航平 <captain.marvel.mk@gmail.com>
2024-08-01 23:29:26 +0900

    CRUD処理の実装 - 途中

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
-use std::env;
+use tower::util::Optional;
+use std::sync::{Arc, RwLock};
+use std::{collections::HashMap, env};
 use std::net::SocketAddr;
 
 use axum::{
@@ -8,6 +10,86 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum RepositoryError {
+  #[error("Not Found, id is {0}")]
+  NotFound(i32)
+}
+
+pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+  fn create(&self, payload: CreateTodo) -> Todo;
+  fn find(&self, id: i32) -> Option<Todo>;
+  fn all(&self) -> Vec<Todo>;
+  fn update(&self, id:i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
+  fn delete(&self, id: i32) -> anyhow::Result<()>;
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+pub struct Todo {
+  id: i32,
+  text: String,
+  completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+pub struct CreateTodo {
+  text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+pub struct UpdateTodo {
+  text: Option<String>,
+  completed: Option<bool>
+}
+
+impl Todo {
+  pub fn new(id: i32, text: String) -> Self {
+    Self {
+      id,
+      text,
+      completed: false
+    }
+  } 
+}
+
+type TodoData = HashMap<i32, Todo>;
+
+#[derive(Clone, Debug)]
+pub struct TodoRepositoryForMemory {
+  store: Arc<RwLock<TodoData>>
+}
+
+impl TodoRepositoryForMemory {
+    pub fn new() -> Self {
+      TodoRepositoryForMemory {
+        store: Arc::default(),
+      }
+    }
+}
+
+impl TodoRepository for TodoRepositoryForMemory {
+  fn create(&self, payload: CreateTodo) -> Todo {
+    todo!();
+  }
+
+  fn find(&self, id: i32) -> Option<Todo> {
+    todo!();
+  }
+
+  fn all(&self) -> Vec<Todo> {
+    todo!();
+  }
+
+  fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+    todo!();
+  }
+
+  fn delete(&self, id:i32) -> anyhow::Result<()> {
+    todo!();
+  }
+}
 
 #[tokio::main]
 async fn main() {
